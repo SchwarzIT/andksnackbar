467484ad170972bbf6069e2b965fea828649e307
Victor Kostadinovski <victor.kostadinovski@kaufland.de>
2017-07-25 15:43:58 +0300

    #Added more tests in SnackbarAdapter class.

---
+++ library/src/test/java/kaufland/com/snackbarlibrary/view/SnackbarAdapterTest.java
@@ -0,0 +1,77 @@
+package kaufland.com.snackbarlibrary.view;
+
+import android.view.View;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({SnackbarAdapter.class, SnackbarAdapter.ViewHolder.class})
+public class SnackbarAdapterTest {
+
+    private SnackbarAdapter mAdapter;
+
+    private SnackbarView viewMock;
+
+
+    private SnackbarAdapter.ViewHolder mViewHolder;
+
+    @Before
+    public void init() throws Exception {
+        mAdapter = PowerMockito.spy(new SnackbarAdapter());
+        View holderView = Mockito.mock(View.class);
+        mViewHolder = PowerMockito.spy(new SnackbarAdapter.ViewHolder(holderView));
+        PowerMockito.doNothing().when(mAdapter).notifyItemInserted(Mockito.anyInt());
+        PowerMockito.doNothing().when(mAdapter).notifyItemRemoved(Mockito.anyInt());
+        viewMock = Mockito.mock(SnackbarView.class);
+    }
+
+    @Test
+    public void testZeroAdapterSize(){
+        Assert.assertThat(mAdapter.isEmpty(), Matchers.is(true));
+    }
+
+
+    @Test
+    public void testOnCreateViewHolder() throws Exception {
+
+        mAdapter.addItem(viewMock);
+        PowerMockito.whenNew(SnackbarAdapter.ViewHolder.class).withAnyArguments().thenReturn(mViewHolder);
+
+        mAdapter.onCreateViewHolder(null, 0);
+
+        Mockito.verify(viewMock, Mockito.times(1)).onCreateView(null);
+    }
+
+    @Test
+    public void testOnBindViewHolder() throws Exception{
+
+        mAdapter.addItem(viewMock);
+        PowerMockito.whenNew(SnackbarAdapter.ViewHolder.class).withAnyArguments().thenReturn(mViewHolder);
+        mAdapter.onCreateViewHolder(null, 0);
+        mAdapter.onBindViewHolder(mViewHolder,0);
+    }
+
+    @Test
+    public void testAddItem(){
+        mAdapter.addItem(viewMock);
+        Assert.assertEquals(mAdapter.getItemCount(),1);
+
+    }
+
+    @Test
+    public void testRemoveItem(){
+        mAdapter.addItem(viewMock);
+        Assert.assertEquals(mAdapter.getItemCount(),1);
+        mAdapter.removeItem(viewMock);
+        Assert.assertEquals(mAdapter.getItemCount(),0);
+    }
+}
